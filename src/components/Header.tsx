396730a3c6517f9bf2246cc2a3084e184286457e
Thiago <thiagofreitascarneiro@hotmail.com>
2022-03-24 21:47:58 -0300

    creating dashboard and graphic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, Stack, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotification2Line, RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -45,7 +45,38 @@ export function Header() {
                     border='none'
                 />
                <Icon as={RiSearchLine} fontSize='20' />
+            
+            </Flex>
+
+            <Flex
+                align='center'
+                ml='auto'
+               >
+                   <HStack 
+                   spacing='4'
+                   mx='8'
+                   pr='8'
+                   py='1'
+                   color='gray.300'
+                   borderRightWidth={1}
+                   borderColor='gray.700'
+                   >
+                        <Icon as={RiNotificationLine} fontSize="20" />
+                        <Icon as={RiUserAddLine} fontSize="20" ml='4' />
+                   </HStack>
                 
+                <Flex align='center'>
+                    <Box mr='4' textAlign='right'>
+                       <Text>Thiago Carneiro </Text>
+                       <Text 
+                       color="gray.300" 
+                       fontSize="small"> 
+                        thiagofreitascarneiro@hotmail.com
+                       </Text> 
+                    </Box>
+
+                    <Avatar size="md" name="Thiago Carneiro" src="https://github.com/thiagofreitascarneiro.png" />
+                </Flex>
             </Flex>
         </Flex>
     )
