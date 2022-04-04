c881a629a11c31653b99a9678925ec29c6ad88e0
Thiago <thiagofreitascarneiro@hotmail.com>
2022-04-04 20:44:54 -0300

    aplying query mutations in the project"

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,11 +2,15 @@ import { Box, Button, Text, Divider, Flex, Heading, HStack, SimpleGrid, VStack} 
 import { Input } from "../../components/Form/input";
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from 'next/link'
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 
 type CreateUserFormData = {
@@ -29,6 +33,24 @@ const createUserFormSchema = yup.object().shape({
 
 
 export default function CreateUser() {
+    const router = useRouter()
+
+    //executa uma função de forma asycrona
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+
+        return response.data.user;
+    }, {
+        // se der sucesso eu quero limpar os dados do cache
+       onSuccess: () => {
+           queryClient.invalidateQueries('users')
+       } 
+    });
 
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
@@ -37,7 +59,9 @@ export default function CreateUser() {
     const { errors } = formState
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        await createUser.mutateAsync(values);
+
+        router.push('/users')
     }
 
     return (
